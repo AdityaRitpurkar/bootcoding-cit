47a0e3accfb0ccc8131937a71d870e1a305ed033
AdityaRitpurkar <adityaritpurkar@gmail.com>
2023-08-17 15:50:32 +0530

    java programs beginner

---
+++ src/java/hour.java
@@ -0,0 +1,13 @@
+public class hour {
+    public static void main(String[] args) {
+        int hours=1;
+        int minutes=2;
+        int seconds = minutes*60;
+        int hourseconds =hours*60*60;
+
+        System.out.println("hours to second :"+hourseconds);
+        System.out.println("minutes to second :"+seconds);
+
+
+    }
+}
