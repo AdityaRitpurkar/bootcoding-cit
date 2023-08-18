256426a036b7f0ec0489e1fa278c60b0723b3c18
AdityaRitpurkar <adityaritpurkar@gmail.com>
2023-08-18 12:43:27 +0530

    java programs beginner

--- src/java/inputEx.java
+++ src/java/inputEx.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class inputEx {
     public static void main(String[] args) {
 
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter first number:");
         int n = sc.nextInt();
